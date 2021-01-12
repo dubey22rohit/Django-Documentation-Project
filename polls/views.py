879d4f4e2b980f530ae29364d16aad36f5c4fc30
Rohit Dubey <dubey22rohit@gmail.com>
2021-01-12 18:34:34 +0530

    Added Styles

--- polls/views.py
+++ polls/views.py
@@ -13,12 +13,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_questions_list'
 
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[;5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-    return Question.objects.filter(pub_date__lte=timezone.now())
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
