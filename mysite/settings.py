d0ea3e184167462a612358992c5ad668d394da0b
Rohit Dubey <dubey22rohit@gmail.com>
2021-01-12 20:42:23 +0530

    Added Styles

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'votingsystem',
         'USER': 'postgres',
-        'PASSWORD': 'nvidia990',
+        'PASSWORD': 'yourpassword',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
